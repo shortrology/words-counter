f22da2b6b2837b97cc0b921ba0de34c4e727250a
shortrology <alyanaalialyanaali@gmail.com>
2024-05-30 20:19:49 +0500

    This is a words counter

---
+++ index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// Declare a constant 'answers' and assign it to the result of inquirer.prompt function
+
+const answers: {
+  sentence: string;
+} = await inquirer.prompt([
+  {
+    name: "sentence",
+    type: "input",
+    message: "Enter your sentence to count the word:",
+  },
+]);
+const words = answers.sentence.trim().split(" ");
+
+//Print the array of words to the console
+console.log(words);
+
+// Print the word count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
